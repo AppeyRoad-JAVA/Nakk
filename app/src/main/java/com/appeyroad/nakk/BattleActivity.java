23e8d34a18767dcf70dd40c6e8b9a84e774bb0e4
LeeSeokJun <lsj574@snu.ac.kr>
2016-07-17 19:33:14 +0900

    저작권 정보 업데이트
    기본 파일 헤더는 사용하지 않으며 COPYING 정보를 최상단에 작성함

--- app/src/main/java/com/appeyroad/nakk/BattleActivity.java
+++ app/src/main/java/com/appeyroad/nakk/BattleActivity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2016  AppeyRoad-JAVA team
+ *
+ * This file is part of Nakk.
+ *
+ * Nakk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nakk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nakk.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.appeyroad.nakk;
 
 import android.app.Activity;
@@ -12,9 +31,6 @@ import android.widget.RelativeLayout;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * Created by yskim_000 on 2016-07-16.
- */
 public class BattleActivity extends Activity{
     public ProgressBar gaugeBar;
     public boolean onBattle;
