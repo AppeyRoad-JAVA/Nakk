0f22a5ad5744dfc011f28b4b92d3be77fd81f1a3
Kim Yoonsung <yskim227@snu.ac.kr>
2016-07-19 19:44:03 +0900

    전투진행 방식 변경
    단순히 화면을 클릭하는 것이 아니라 장력, 중력, 물고기의 힘, 체력, 거리 등을 추가해서 좀 더 흥미롭게 바꿈.

--- app/src/main/java/com/appeyroad/nakk/BattleActivity.java
+++ app/src/main/java/com/appeyroad/nakk/BattleActivity.java
@@ -22,33 +22,44 @@ package com.appeyroad.nakk;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class BattleActivity extends AppCompatActivity{
-    private ProgressBar gaugeBar;
+    private ProgressBar tensionBar;
     private boolean onBattle;
     //private Button button;
+    private Button button2;
     private ImageView imageView1;
     private ImageView imageView2;
+    private TextView textView;
 
-    private TimerTask pull;
+    private TimerTask battle;
     private TimerTask stuck;
     private Handler handler = new Handler();
 
+    private double maxStrength;
+    private double strength;
+    private double weight;
+    private double distance;
+    private double tension;
+    private double maxHp;
+    private double hp;
+    private boolean reeling;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
 
-        gaugeBar = (ProgressBar) findViewById(R.id.progressBar_battle_gauge);
-        gaugeBar.setVisibility(View.INVISIBLE);
+        tensionBar = (ProgressBar) findViewById(R.id.progressBar_battle_tension);
+        tensionBar.setVisibility(View.INVISIBLE);
         onBattle = false;
 
         imageView1 = (ImageView) findViewById(R.id.imageView_battle_rod_normal);
@@ -67,19 +78,6 @@ public class BattleActivity extends AppCompatActivity{
             @Override
             public void onClick(View view){
                 finish();
-            }
-        });
-
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout_battle);
-        layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(onBattle){
-                    gaugeBar.setProgress(gaugeBar.getProgress() + 100);
-                    if(gaugeBar.getProgress()>=gaugeBar.getMax()){
-                        endBattle(true);
-                    }
-                }
             }
         });
 
@@ -102,50 +100,106 @@ public class BattleActivity extends AppCompatActivity{
                 timer.scheduleAtFixedRate(stuck, 0, 100);
             }
         });
+        button2 = (Button) findViewById(R.id.button_battle_reel);
+        button2.setOnTouchListener(new View.OnTouchListener() {
+            TimerTask reel;
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                    reeling=true;
+                }
+                else if(event.getAction()==MotionEvent.ACTION_UP){
+                    reeling=false;
+                }
+                return false;
+            }
+        });
+        textView=(TextView) findViewById(R.id.textView_battle_debug);
     }
     private void beginBattle() {
-        if(onBattle==true){
+        if(onBattle){
             return;
         }
         onBattle = true;
         handler.post(new Runnable() {
             @Override
             public void run() {
-                gaugeBar.setVisibility(View.VISIBLE);
-                //button.setEnabled(true);
+                tensionBar.setVisibility(View.VISIBLE);
+                //button.setEnabled(false);
+                button2.setEnabled(true);
                 imageView1.setVisibility(View.INVISIBLE);
                 imageView2.setVisibility(View.VISIBLE);
+                tensionBar.setMax(400);
+                tensionBar.setProgress(0);
             }
         });
-        gaugeBar.setMax(1000);
-        gaugeBar.setProgress(200);
+
+        distance=1050;
+        maxStrength=150;
+        strength=maxStrength;
+        weight=100;
+        tension=200;
+
+        maxHp=1000;
+        hp=maxHp;
 
         Timer timer = new Timer();
-        pull = new TimerTask() {
+        battle = new TimerTask() {
             public void run() {
-                gaugeBar.setProgress(gaugeBar.getProgress() - 40);
-                if (gaugeBar.getProgress() <= 0) {
-                    endBattle(false);
+                distance-=(tension-strength)/10;
+                if(distance<50) endBattle(true);
+
+                strength = (maxStrength*hp)/maxHp+weight;
+
+                if(reeling) {
+                    tension += Math.abs(tension - 200) / 20 + 10;
                 }
+                else {
+                    if(tension>strength){
+                        tension-=Math.abs(tension - strength)/4+20;
+                    }
+                    else {
+                        tension -= 5;
+                    }
+                }
+
+                if(tension>400 || tension<strength/4)
+                    endBattle(false);
+                if(tension>=200)
+                    hp-=(tension-200)/5;
+                if(hp<0)    hp=0;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("");
+                        textView.append("distance: "+(int)distance);
+                        textView.append("\ntension: "+(int)tension);
+                        textView.append("\nstrength: "+(int)strength);
+                        textView.append("\nhp: "+(int)hp);
+                        tensionBar.setProgress((int)tension);
+                    }
+                });
             }
         };
-        timer.scheduleAtFixedRate(pull, 0, 500);
+        timer.scheduleAtFixedRate(battle, 0, 100);
     }
     private void endBattle(boolean win){
-        if(onBattle==false){
+        if(!onBattle){
             return;
         }
         onBattle = false;
         handler.post(new Runnable() {
             @Override
             public void run() {
-                gaugeBar.setVisibility(View.INVISIBLE);
+                tensionBar.setVisibility(View.INVISIBLE);
                 //button.setEnabled(true);
+                button2.setEnabled(false);
                 imageView1.setVisibility(View.VISIBLE);
                 imageView2.setVisibility(View.INVISIBLE);
             }
         });
 
-        pull.cancel();
+        reeling=false;
+        battle.cancel();
     }
 }
