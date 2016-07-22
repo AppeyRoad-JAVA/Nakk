269417a93e67c6d7975c1f5a14f0b26a18557dbc
Nam Giwon <nocturn7721@gmail.com>
2016-07-22 20:28:20 +0900

    Add shop activity
    Add Class ShopActivity and activity_shop.xml

---
+++ app/src/main/java/com/appeyroad/nakk/ShopActivity.java
@@ -0,0 +1,9 @@
+package com.appeyroad.nakk;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 남기원 on 2016-07-22.
+ */
+public class ShopActivity extends AppCompatActivity{
+}
